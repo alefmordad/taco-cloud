8b26f94bc49adf8fc38632fb2d58f8cc56d4cdde
alef mordad <alefmordad@gmail.com>
2020-11-18 15:13:09 +0330

    chapter2: refactor and disable template parse cache

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -7,9 +7,9 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import tacos.Ingredient;
-import tacos.Ingredient.Type;
-import tacos.Taco;
+import tacos.model.Ingredient;
+import tacos.model.Ingredient.Type;
+import tacos.model.Taco;
 
 import javax.validation.Valid;
 import java.util.Arrays;

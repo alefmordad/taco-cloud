4e8c8243a1e552be426d671bd8ef935bfdc3581f
mjdarsanj <mjdarsanj@gmail.com>
2020-11-25 01:05:04 +0330

    chapter4: 4.3.2

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,10 +23,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	// somehow it configures authentication
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService)
 				.passwordEncoder(encoder());
+	}
+
+	// somehow it configures authorization
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// order of calling antMatchers is important
+		// some other options are (instead of permitAll or hasRole(String)):
+		// access(String): evaluates its SpEL expression using other options and authentication and principal
+		// anonymous(): allow access to anonymous users
+		// authenticated(): allow access to authenticated users
+		// denyAll(): no one
+		// permitAll(): every one
+		// fullyAuthenticated(): if user is not anonymous nor remember-me (cookie)
+		// rememberMe()
+		// hasAnyAuthority(String...) - hasAuthority(String)
+		// hasAnyRole(String...) - hasRole(String)
+		// hasIpAddress(String)
+		// not()
+
+		// for authentication just in Tuesdays
+//				.access("hasRole('ROLE_USER') " +
+//						"&& T(java.util.Calendar).getInstance().get(T(java.util.Calendar).DAY_OF_WEEK) " +
+//						"== T(java.util.Calendar).TUESDAY")
+
+		// and() means current authorization configuration is finished
+		// .usernameParameter("user").passwordParameter("pwd") to customize user pass attribute names
+		// .defaultSuccessUrl("/design", true); to force user to navigate even if he requested some other url
+		http.authorizeRequests()
+				.antMatchers("/design", "/orders").access("hasRole('ROLE_USER')")
+				.antMatchers("/", "/**").access("permitAll()")
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/design")
+				.and()
+				.logout()
+				.logoutSuccessUrl("/");
 	}
 
 }
--- src/main/java/tacos/web/WebConfig.java
+++ src/main/java/tacos/web/WebConfig.java
@@ -6,11 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // another way for resolving view for request instead of writing simple controllers
 @Configuration
-public class HomeConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/login");
 	}
 
 }
